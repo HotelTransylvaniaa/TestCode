8f76c340cee2fe15925108d2242add4a87772d6b
MarwaAhmeed <ahmedmarwa673@gmail.com>
2022-03-26 23:06:20 +0200

    search

--- client/src/booking/Hotels.js
+++ client/src/booking/Hotels.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import HotelCard from "../components/cards/HotelCard";
 import { getHotelsList } from "../store/actions/hotels";
 import Filter from "../components/Filter";
+import Search from "../components/Forms/search";
 export default function Hotels() {
   let hotels = useSelector((state) => state.hotels.hotelsList);
    const dispatch = useDispatch();
@@ -12,9 +13,10 @@ export default function Hotels() {
   }, []);
   return (
     <div className="container">
+      <Search/>
       <div className="row">
         <div className="col-4 shadow mt-3">
-          <Filter/>
+          {/* <Filter/> */}
           </div>
         <div className="col-8">
           <div className="container-fluid">
--- server/controllers/hotels.js
+++ server/controllers/hotels.js
@@ -14,11 +14,6 @@ const findByCityAndRating = (city) => {
   return Hotels.find({"address.city":city,rating:5});
 };
 
-//for search listing
-// const findBysearchData = (body) => {
-//      console.log(body);
-//   return true;
-// };
 // get all hotels in city
 const findByCity = (city) => {
   console.log(city)
@@ -44,12 +39,17 @@ const searchListings = async (req, res) => {
   const {location, date ,room} = req.body
    console.log(location, date ,room)
   console.log(location);
+  const city=location.split(",");
+  console.log(city[0])
+  const roomType=room.toLowerCase();
+  console.log(roomType)
+
   //const fromDate = date.split(',');
   // console.log(fromDate[0]);
  // let result = await Hotels.find({from: {$gte: new Date(FormData[0])}, location }).select('-image.data').exec();
-  let result = await Hotels.find({"address.city" :location }).select('-image.data').exec();
+  let result = await Hotels.find({"address.city" :city[0] ,"rooms.roomType":roomType}).exec();
   console.log(result);
-  //res.json(result);
+  res.json(result);
 }
 
 module.exports = {
