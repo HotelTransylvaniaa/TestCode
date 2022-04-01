49040d5e1016b1164f1aafbcf43853504b6f0ba2
shehabkhallaf <shehabkhallaf122@gmail.com>
2022-04-01 18:26:46 +0200

    serach location

--- client/src/components/Forms/search.js
+++ client/src/components/Forms/search.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import { SearchOutlined } from "@ant-design/icons";
 import AlgoliaPlaces from "algolia-places-react";
 import DatePicker from "react-multi-date-picker";
@@ -11,21 +10,11 @@ import "../header.css";
 const { RangePicker } = DatePicker;
 const { option } = Select;
 
-// const config = {
-//   appId: process.env.REACT_APP_ALGOLIA_ID,
-//   apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
-//   language: "en",
-// };
-
-const city = [
-  "Cairo",
-  "Alexandria",
-  "Asswan",
-  "Sharm El Sheikh",
-  "Hurghada",
-  "Giza",
-  "Luxor",
-];
+const config = {
+  appId: process.env.REACT_APP_ALGOLIA_ID,
+  apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
+  language: "en",
+};
 
 const Search = () => {
   const navigate = useNavigate();
@@ -33,18 +22,6 @@ const Search = () => {
   const [checkInDate, setCheckInDate] = useState(null);
   const [checkOutDate, setCheckOutDate] = useState(null);
   const [room, setRoom] = useState("");
-  const [searchTerm, setSearchTerm] = React.useState("");
-  const [searchResults, setSearchResults] = React.useState([]);
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-  };
-
-  React.useEffect(() => {
-    const results = city.filter(person =>
-      person.toLowerCase().includes(searchTerm)
-    );
-    setSearchResults(results);
-  }, [searchTerm]);
 
   const rooms = [{ label: "Single" }, { label: "Double" }, { label: "Triple" }];
 
@@ -75,47 +52,54 @@ const Search = () => {
     );
   }
 
-  // (function () {
-  //   var placesAutocomplete = ({
-  //     appId: process.env.REACT_APP_ALGOLIA_ID,
-  //     apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
-  //     container: document.querySelector("#city"),
-  //     templates: {
-  //       value: function (suggestion) {
-  //         return suggestion.name;
-  //       },
-  //     },
-  //   }).configure({
-  //     type:'address'
-  //   })
-  //   placesAutocomplete.on('change',function resultSelected(e){
-  //     document.querySelector('#state').value =e.suggestion.adminstrative || '';
-  //   })
-  // })();
-
   return (
     <>
       <div className="searchBar p-0 mt-5">
         <div className="container my-1">
           <div className="row gx-2 py-4 justify-content-center">
             <div className="col-sm-12 col-md-4">
-              {/* <AlgoliaPlaces
+              <input
+                type="text"
+                id="wizards"
+                name="wizards"
+                list="wizards-list"
+                style={{ height: "50px" }}
                 placeholder="location"
+                className="form-control"
                 defaultValue={location}
                 option={config}
-                onChange={({ suggestion }) => setLocation(suggestion.value)}
-                style={{ height: "50px" }}
-              /> */}
-              {/* <input
-                type="search"
-                id="city"
-                className="form-control"
-                placeholder="location"
-                style={{ height: "50px" }}
-                
-              /> */}
-              <input type="search" className="form-control" placeholder="location" style={{ height: "50px" }} value={searchTerm}
-        onChange={handleChange}/>
+                onChange={(e) => setLocation(e.target.value)}
+              />
+
+              <datalist id="wizards-list">
+                <option>Cairo</option>
+                <option>Alexandria</option>
+                <option>Asswan</option>
+                <option>Sharm El Sheikh</option>
+                <option>Hurghada</option>
+                <option>Giza</option>
+                <option>Assyut</option>
+                <option>Luxor</option>
+                <option>Minia</option>
+                <option>Marsa Matroh</option>
+                <option>Dahb</option>
+                <option>Gona</option>
+                <option>Marsa Allam</option>
+                <option>Qena</option>
+                <option>Bani Suef</option>
+                <option>Port Said</option>
+                <option>Suez</option>
+                <option>Al-Mansura</option>
+                <option>Ismailia</option>
+                <option>Sohag</option>
+                <option>6th of October</option>
+                <option>Gona</option>
+                <option>Gona</option>
+                <option>Gona</option>
+
+
+
+              </datalist>
             </div>
             <div className="col-sm-12 col-md-2">
               <DatePicker
