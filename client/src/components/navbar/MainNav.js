a64ae90a484f14c58c0275c82015e24e58387d7b
BlackyTreasure <abahaamadany@gmail.com>
2022-04-02 10:22:41 +0200

    navbar fix

--- client/src/components/navbar/MainNav.js
+++ client/src/components/navbar/MainNav.js
@@ -7,38 +7,30 @@ export default function Navbar() {
     const { auth } = useSelector((state) => ({ ...state }))
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light nav-z">
             <div className="container-fluid">
-                <NavLink className="navbar-brand ms-4" to={"/"}>
-                    <img src="https://cdn6.agoda.net/images/kite-js/logo/agoda/color-default.svg" alt="" style={{ width: '75px' }} />
-                </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse text-end" id="navbarSupportedContent">
+                <NavLink className="navbar-brand mx-auto" to={"/"}>
+                    <img src="https://cdn6.agoda.net/images/kite-js/logo/agoda/color-default.svg" alt="" style={{ width: '75px' }} />
+                </NavLink>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mb-2 mb-lg-0">
                         <li className="nav-item me-2 fs-5">
-                        <Link className="nav-link text-end"  to={"/"} exact="true">Home</Link>
+                            <Link className="nav-link" to={"/"} exact="true">Home</Link>
                         </li>
                         <li className="nav-item me-2 fs-5">
-                        <Link className="nav-link text-end"  to={"/hotels"} exact="true">Hotels</Link>
+                            <Link className="nav-link" to={"/hotels"} exact="true">Hotels</Link>
                         </li>
                         <li className="nav-item me-2 fs-5">
-                        <Link className="nav-link text-end"  to={"/contact-us"} exact="true">Contact Us</Link>
+                            <Link className="nav-link" to={"/contact-us"} exact="true">Contact Us</Link>
+                        </li>
+                        <li className="nav-item my-auto">
+                            {auth !== null && (<NavAccount />)}
+                            {auth === null && (<NavSign />)}
                         </li>
                     </ul>
-                    {/* <div className="navUl d-flex justify-content-center align-items-center me-auto">
-                        <NavLink className="me-3 text-decoration-none" to={"/"} exact="true">Home</NavLink>
-                        <NavLink className="me-3 text-decoration-none" to={"/hotels"} exact="true">Hotels</NavLink>
-                        <NavLink className="me-3 text-decoration-none" to={""}>Resorts</NavLink>
-                        <NavLink className="me-3 text-decoration-none" to={""}>Today’s deals</NavLink>
-                        <NavLink className="me-3 text-decoration-none" to={""}>About</NavLink>
-                        <NavLink className="me-3 text-decoration-none" to={"/contact-us"}>Contact Us</NavLink>
-                    </div> */}
-                    <div className="d-flex ms-auto text-end">
-                        {auth !== null && (<NavAccount />)}
-                        {auth === null && (<NavSign />)}
-                    </div>
                 </div>
             </div>
         </nav>
--- client/src/components/navbar/NavSign.js
+++ client/src/components/navbar/NavSign.js
@@ -1,10 +1,10 @@
 import './NavSign.css'
 import { NavLink } from "react-router-dom";
-export default function NavSign(){
-    return(
-        <div className='navSign'>
-        <NavLink className="text-decoration-none d-inline-block py-2 px-2 border border-primary border-1 rounded me-1" to={"/register"}>Register</NavLink>
-        <NavLink className="text-decoration-none d-inline-block py-2 px-2 border border-primary border-1 rounded ms-1" to={"/login"}>Login</NavLink>
+export default function NavSign() {
+    return (
+        <div className='navSign '>
+            <NavLink className="text-decoration-none d-inline-block py-2 px-2 border border-primary border-1 rounded me-1" to={"/register"}>Register</NavLink>
+            <NavLink className="text-decoration-none d-inline-block py-2 px-2 border border-primary border-1 rounded ms-1" to={"/login"}>Login</NavLink>
         </div>
     )
 }
