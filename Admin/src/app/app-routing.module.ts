0aefbc86757a64d1357d3317167ed26d2fa475cd
BlackyTreasure <abahaamadany@gmail.com>
2022-04-03 09:44:02 +0200

    app route hotels

--- Admin/src/app/app-routing.module.ts
+++ Admin/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelsListComponent } from './components/hotels/hotels-list/hotels-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"/admin/hotels",component:HotelsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
