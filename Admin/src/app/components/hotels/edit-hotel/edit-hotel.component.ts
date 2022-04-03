68064193fefead8ca7b403e4dacc95c1ea9e5e94
BlackyTreasure <abahaamadany@gmail.com>
2022-04-03 22:32:44 +0200

    hotels

--- Admin/src/app/components/hotels/edit-hotel/edit-hotel.component.ts
+++ Admin/src/app/components/hotels/edit-hotel/edit-hotel.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HotelsService } from 'src/app/services/hotels.service';
+import { ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-edit-hotel',
   templateUrl: './edit-hotel.component.html',
   styleUrls: ['./edit-hotel.component.scss']
 })
 export class EditHotelComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private service:HotelsService,private activatedRoute: ActivatedRoute) { }
+  Hotels:any
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      const HotelId = params['id'];
+      this.service.getHotelById(HotelId).subscribe(
+        (res)=>this.Hotels=res,
+        (err)=> {console.log(err)})
+    });
   }
 
 }
