387e2205044c9f4bb9f5348cfa21d2f985ed2ed5
MarwaAhmeed <ahmedmarwa673@gmail.com>
2022-04-03 13:06:29 +0200

    delet editUser

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -5,10 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './components/users/users-list/users-list.component';
 import { AddUserComponent } from './components/users/add-user/add-user.component';
-import { EditUserComponent } from './components/users/edit-user/edit-user.component';
 import { HotelsListComponent } from './components/hotels/hotels-list/hotels-list.component';
 import { AddHotelComponent } from './components/hotels/add-hotel/add-hotel.component';
-import { EditHotelComponent } from './components/hotels/edit-hotel/edit-hotel.component';
 import { BookingListComponent } from './components/booking/booking-list/booking-list.component';
 import { EditBookingComponent } from './components/booking/edit-booking/edit-booking.component';
 import { AddBookingComponent } from './components/booking/add-booking/add-booking.component';
@@ -21,10 +19,8 @@ import { DashboardComponent } from './components/dashboard/dashboard/dashboard.c
     AppComponent,
     UsersListComponent,
     AddUserComponent,
-    EditUserComponent,
     HotelsListComponent,
     AddHotelComponent,
-    EditHotelComponent,
     BookingListComponent,
     EditBookingComponent,
     AddBookingComponent,
