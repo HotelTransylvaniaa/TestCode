262722f34127ee28b82fb17eaa30b8323fb06ca2
MarwaAhmeed <ahmedmarwa673@gmail.com>
2022-04-05 16:46:19 +0200

    booking

--- Admin/src/app/components/booking/booking-list/booking-list.component.ts
+++ Admin/src/app/components/booking/booking-list/booking-list.component.ts
@@ -8,26 +8,24 @@ import {MatTreeModule} from '@angular/material/tree';
   styleUrls: ['./booking-list.component.scss']
 })
 export class BookingListComponent implements OnInit {
-  booking:any
-  
+  booking:any;
+
   constructor(private service:BookingService) { }
 
   ngOnInit(): void {
     this.getbooking()
-    
+
   }
   del(id:any){
     this.service.deletebookingList(id).subscribe(
       (res)=>this.getbooking(),
-      
       (err)=> {console.log(err)})
-      
+
   }
   getbooking()
   {
     this.service.getbookingList().subscribe(
       (res)=>this.booking=res,
-      
       (err)=> {console.log(err)})
   }
 
