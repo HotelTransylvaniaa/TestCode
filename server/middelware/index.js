351570cc46994b0b8a173b87943a2deb95c4e622
MarwaAhmeed <ahmedmarwa673@gmail.com>
2022-04-07 22:07:01 +0200

    utils

--- server/middelware/index.js
+++ server/middelware/index.js
@@ -6,3 +6,21 @@ const expressJwt =require("express-jwt")
 })
 
 module.exports=requireSignin
+
+
+
+const {promisify}=require("util")
+const jwt=require("jsonwebtoken")
+const User=require("../models/users")
+const verify=promisify(jwt.verify);
+
+const auth=async(req,res,next)=>{
+ const {authorization}=req.headers
+ const user=await verify(authorization,"ggggygygygygygygygyygydssss").catch((err)=>{res.status(401).end()})
+ req.user=await User.findById(user.userId)
+ console.log(req.user)
+ next()
+}
+
+
+module.exports=auth;
