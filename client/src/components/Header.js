d0b6933c0f6249861399aeb9fa51841eb1f59c3f
shehabkhallaf <shehabkhallaf122@gmail.com>
2022-04-15 06:09:24 +0200

    home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,16 @@
+import "./header.css";
 import React from "react";
 
 const Header = () => {
-
   return (
     <>
-        <img 
+      <div className="header">
+        <img
           // src="https://www.orbitaltravel.co.uk/application/files/6115/4808/7958/Complex.jpg"
           src="https://ld-wt73.template-help.com/wt_prod-20476/images/slider-4-slide-1-1920x678.jpg"
           // src="https://ld-wt73.template-help.com/wt_prod-20476/images/slider-4-slide-2-1920x678.jpg"
           // src="https://ld-wt73.template-help.com/wt_prod-20476/images/slider-4-slide-3-1920x678.jpg"
-          height="550px"
-          width="100%"
+          
         />
         {/* <img 
           src="https://cdn.pixabay.com/photo/2016/08/13/13/18/palace-1590716_960_720.jpg"
@@ -22,11 +22,17 @@ const Header = () => {
           height="550px"
           width="100%"
         /> */}
-         {/* <img 
+        {/* <img 
           src="https://pix6.agoda.net/geo/city/6700/1_6700_02.jpg?ca=6&ce=1&s=1920x822"
           height="500px"
           width="100%"
         /> */}
+
+        <div className="header-content text-white py-4 px-3">
+          <h2>Welcome to AGODA </h2>
+          <h5>Pay less with Agoda so you can spend more on yourself.</h5>
+        </div>
+      </div>
     </>
   );
 };
