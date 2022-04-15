9ece8ae930737c6cf9cdb0442ce046b46968e9c9
BlackyTreasure <abahaamadany@gmail.com>
2022-04-16 00:12:57 +0200

    delete user booking

--- Admin/src/app/components/users/user-booking/user-booking.component.ts
+++ Admin/src/app/components/users/user-booking/user-booking.component.ts
@@ -1,5 +1,6 @@
 import { ActivatedRoute } from '@angular/router';
 import { UsersService } from 'src/app/services/users.service';
+import { BookingService } from 'src/app/services/booking.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,19 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class UserBookingComponent implements OnInit {
   activeID:number=0;
   booking:any;
-  constructor(private service:UsersService,private activatedRoute:ActivatedRoute) {
+  constructor(private service:UsersService,private activatedRoute:ActivatedRoute,private service2:BookingService ) {
     this.activeID = this.activatedRoute.snapshot.params['id'];
     console.log(this.activeID);
    }
 
   ngOnInit(): void {
+    this.getUserBooking()
+  }
+  deleteUserBooking(id:any){
+    this.service2.deletebookingList(id).subscribe(
+      (res)=>this.getUserBooking(),
+      (err)=> {console.log(err)})
+  }
+
+  getUserBooking(){
     this.service.userBooking(this.activeID).subscribe(
       (res)=>this.booking=res,
       (err)=> {console.log(err)
       })
   }
-  deleteUserBooking(id:any){
-     console.log(id)
-  }
-
 }
